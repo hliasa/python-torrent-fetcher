d8a01ff6c47ac647a720ac0ffd148fc7609ebd94
hliasa <hliasant@gmail.com>
2015-11-25 22:51:02 +0200

    improved results view

--- python-torrent-fetcher.py
+++ python-torrent-fetcher.py
@@ -16,22 +16,24 @@ url = "http://torrentproject.org/?s="+search_title+"&out=json&num=150"
 response = urlopen(url).read().decode('utf8')
 obj = json.loads(response)
 total_found = int(obj.get('total_found'))
-print("Total torrents found:",total_found)
 
 if total_found > 150:
-	search_max = 150
-	print("Result number exceeds server's limit (150). Printing 150 first results...")
+    search_max = 150
 else:
-	search_max = total_found
+    search_max = total_found
+print("==============================RESULTS==============================")
+for i in range(search_max, 0, -1):
+    print(i,")",obj.get(str(i), {}).get("title")," | Size:", hbytes(obj.get(str(i), {}).get("torrent_size")), "Seeds:", obj.get(str(i), {}).get("seeds"))
+print("==============================RESULTS==============================")
 
-for i in range(1, search_max+1):
-	print(i,")",obj.get(str(i), {}).get("title")," | Size:", hbytes(obj.get(str(i), {}).get("torrent_size")), "Seeds:", obj.get(str(i), {}).get("seeds"))
-
+if total_found > 150:
+    print("Result number exceeds server's limit (150). Printed 150 first results in descending order.")
+print("Total torrents found:",total_found)
 valid_choice = 0
 while valid_choice==0:
-	choice = str(input("Type in the number of the torrent you want to download:"))
-	if 0 < int(choice) < search_max :
-		valid_choice = 1
+    choice = str(input("Type in the number of the torrent you want to download:"))
+    if 0 < int(choice) < search_max :
+        valid_choice = 1
 
 choice_title = obj.get(choice, {}).get("title")
 choice_hash = obj.get(choice, {}).get("torrent_hash")
@@ -41,7 +43,7 @@ response = urlopen(trackers_url).read().decode("utf8")
 obj = json.loads(response)
 torrent_link = "magnet:?xt=urn:btih:"+choice_hash+"&dn="+choice_title
 for i in obj:
-	torrent_link += "&tr="+i
+    torrent_link += "&tr="+i
 print("Launching xdg-open with torrent link..")
 cmd="xdg-open \""+torrent_link+"\""
 os.system(cmd)
